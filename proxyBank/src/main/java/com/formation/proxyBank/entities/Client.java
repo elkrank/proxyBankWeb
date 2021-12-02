297bf7048ae9afd9d71ff4e0a111ec7387e152e0
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-02 23:42:38 +0100

    Update Client.java

--- proxyBank/src/main/java/com/formation/proxyBank/entities/Client.java
+++ proxyBank/src/main/java/com/formation/proxyBank/entities/Client.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Client {
@@ -16,50 +17,72 @@ public class Client {
 	private int codePostal;
 	private String telephone;
 	
+	@ManyToOne
+	private Conseiller conseiller;
+
 //	private CompteCourrant compteCourrant;
 //	private CompteEpargne compteEpargne;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenom() {
 		return prenom;
 	}
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
 	public String getAdresse() {
 		return adresse;
 	}
+
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
+
 	public int getCodePostal() {
 		return codePostal;
 	}
+
 	public void setCodePostal(int codePostal) {
 		this.codePostal = codePostal;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-	
-	
+
+	public Conseiller getConseiller() {
+		return conseiller;
+	}
+
+	public void setConseiller(Conseiller conseiller) {
+		this.conseiller = conseiller;
+	}
+
 	@Override
 	public String toString() {
 		return "Client [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", adresse=" + adresse + ", codePostal="
 				+ codePostal + ", telephone=" + telephone + "]";
 	}
+
 }
