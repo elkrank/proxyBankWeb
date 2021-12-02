b58306f137233916cff174ddaee3cfb3e56a0b3e
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-02 23:42:16 +0100

    Update ClientService.java

--- proxyBank/src/main/java/com/formation/proxyBank/service/ClientService.java
+++ proxyBank/src/main/java/com/formation/proxyBank/service/ClientService.java
@@ -1,5 +1,8 @@
 package com.formation.proxyBank.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,8 @@ import com.formation.proxyBank.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	@Autowired ClientRepository clientRepository;
-
+	@Autowired
+	ClientRepository clientRepository;
 
 	public Client createClient(ClientDto dto) {
 		Client client = new Client();
@@ -22,4 +25,12 @@ public class ClientService {
 		return clientRepository.save(client);
 	}
 
+	public List<Client> getAllClients() {
+		return clientRepository.findAll();
+	}
+	
+	public Client getOneClient(Long id) {
+		Optional<Client> client = clientRepository.findById(id);
+		return client.get();
+	}
 }
