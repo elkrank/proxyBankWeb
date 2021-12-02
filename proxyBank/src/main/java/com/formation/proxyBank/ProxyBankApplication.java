a109d7491e339c5cdcd274fad1cac9adb4ef1d6b
Inacio Eric Lelo Macosso <inacio.macosso@yahoo.com>
2021-12-02 14:35:13 +0100

    Initial commit

---
+++ proxyBank/src/main/java/com/formation/proxyBank/ProxyBankApplication.java
@@ -0,0 +1,13 @@
+package com.formation.proxyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyBankApplication.class, args);
+	}
+
+}
---
+++ proxyBank/src/test/java/com/formation/proxyBank/ProxyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.proxyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
