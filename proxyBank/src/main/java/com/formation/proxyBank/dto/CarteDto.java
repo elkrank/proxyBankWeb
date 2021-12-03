8bbb896d69cab53f10446e772c5b6a8891d0af8e
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-03 21:43:34 +0100

    Create CarteDto.java

---
+++ proxyBank/src/main/java/com/formation/proxyBank/dto/CarteDto.java
@@ -0,0 +1,21 @@
+package com.formation.proxyBank.dto;
+
+public class CarteDto {
+	private String numero;
+	private String typeCarte;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTypeCarte() {
+		return typeCarte;
+	}
+	public void setTypeCarte(String typeCarte) {
+		this.typeCarte = typeCarte;
+	}
+	
+	
+}
