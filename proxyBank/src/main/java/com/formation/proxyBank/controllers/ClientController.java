ca49f83a4e3f2a037c45b44f2782ebd33ed0cf2c
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-03 10:10:14 +0100

    Update application.properties

--- proxyBank/src/main/java/com/formation/proxyBank/controllers/ClientController.java
+++ proxyBank/src/main/java/com/formation/proxyBank/controllers/ClientController.java
@@ -2,10 +2,14 @@ package com.formation.proxyBank.controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +38,14 @@ import com.formation.proxyBank.service.ClientService;
 		public Client getClient(@PathVariable Long id) {
 			return clientService.getOneClient(id);
 		}
+		
+		@Transactional
+		@DeleteMapping("/clients/{id}")
+		public void deleteClient(@PathVariable Long id) {
+			clientService.deleteClient(id);
+		}
+		
+
+		
+
 }
