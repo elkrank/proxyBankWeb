e9867c9902f630e6236f8223cc2c7fe8edd0ce2f
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-03 22:43:41 +0100

    Update CarteController.java

--- proxyBank/src/main/java/com/formation/proxyBank/controllers/CarteController.java
+++ proxyBank/src/main/java/com/formation/proxyBank/controllers/CarteController.java
@@ -1,6 +1,9 @@
 package com.formation.proxyBank.controllers;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.proxyBank.dto.CarteDto;
-import com.formation.proxyBank.dto.ClientDto;
 import com.formation.proxyBank.entities.Carte;
 import com.formation.proxyBank.service.CarteService;
 
@@ -27,5 +29,11 @@ public class CarteController {
 	@GetMapping("/cartes/{id}")
 		public Carte getCarteById(@PathVariable Long id) {
 			return carteService.getCarteById(id);
+	}
+	
+	@Transactional
+	@DeleteMapping("/cartes/{id}")
+		public void deleteCarte(@PathVariable Long id) {
+			carteService.deleteCarte(id);
 	}
 }
