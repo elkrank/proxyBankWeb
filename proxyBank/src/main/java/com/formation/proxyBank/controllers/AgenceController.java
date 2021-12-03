69b8e3447b6deb794ee0c98126fd8ae56666a5ca
rémy Letailleur <r.letailleur@gmail.com>
2021-12-03 10:20:57 +0100

    ajoutEmploye

--- proxyBank/src/main/java/com/formation/proxyBank/controllers/AgenceController.java
+++ proxyBank/src/main/java/com/formation/proxyBank/controllers/AgenceController.java
@@ -3,6 +3,7 @@ package com.formation.proxyBank.controllers;
 import com.formation.proxyBank.entities.Agence;
 import com.formation.proxyBank.service.AgenceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,20 +12,25 @@ public class AgenceController {
     @Autowired
     AgenceService agenceService;
 
-    @PostMapping("/agence")
+    @PostMapping("/agences")
     public Agence createAgence(@RequestBody String name){
        Agence agence = agenceService.createAgence(name);
         return agence;
     }
-    @GetMapping("/agence/{id}/")
+    @GetMapping("/agences/{id}/")
     @ResponseBody
     public Agence getAgenceById(@PathVariable Long id){
         Agence agence = agenceService.findById(id);
         return agence;
     }
-    @PutMapping("/agence/{id}")
+    @PutMapping("/agences/{id}")
     public Agence updateAgence(@PathVariable Long id,@RequestBody Agence agenceP ){
         Agence agence = agenceService.updateAgence(id,agenceP);
         return agence;
+    }
+    @PostMapping("/agences/{id_agence}/ajouterEmploye/?idEmploye")
+    public Agence addEmployeToAgence(@PathVariable Long id_agence,Long idEmploye){
+    Agence agence = agenceService.addEmployeToAgence(id_agence,idEmploye);
+    return agence;
     }
 }
