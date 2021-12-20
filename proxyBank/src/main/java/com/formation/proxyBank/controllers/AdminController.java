268effb03b940e96f5b3330c4c25fa066821aa88
loicbogaert <71258644+loicbogaert@users.noreply.github.com>
2021-12-20 17:30:37 +0100

    fichiers Admin créées

---
+++ proxyBank/src/main/java/com/formation/proxyBank/controllers/AdminController.java
@@ -0,0 +1,35 @@
+package com.formation.proxyBank.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.proxyBank.entities.Admin;
+import com.formation.proxyBank.service.AdminService;
+
+@RestController
+@RequestMapping("/proxybank")
+public class AdminController {
+	@Autowired
+	AdminService adminService;
+
+	@GetMapping("/admin")
+	public List<Admin> getAllAdmin() {
+		return adminService.getAllAdmin();
+	}
+	
+    @DeleteMapping("/admin/{id}")
+	public void deleteAdmin(@PathVariable Long id) {
+		adminService.deleteAdmin(id);
+	}
+    
+    @GetMapping("/admin/{id}") 
+    public Admin getAdminById(@PathVariable Long id){
+    	return adminService.findById(id);
+    }
+}
---
+++ proxyBank/src/main/java/com/formation/proxyBank/entities/Admin.java
@@ -0,0 +1,9 @@
+package com.formation.proxyBank.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Admin {
+
+
+}
